e155d3a4869ddb49eaf577a1ff03b5b8a6c06f37
bartek <bartekpiatek123@gmail.com>
2023-11-01 00:19:23 +0100

    test: working on parsing the data

--- test_data/get_one_frame_from_data.py
+++ test_data/get_one_frame_from_data.py
@@ -1,94 +1,117 @@
 from multiprocessing import Queue
 import struct
 
-filename = "/home/bartek/AGV/AWR1642BOOST_mmWaveRadar/test_data/xwr16xx_processed_stream_2023_10_30T09_00_44_253.dat"
-MAGIC_WORD = b'\x02\x01\x04\x03\x06\x05\x08\x07'
-HEADER_SIZE = 40
 
-with open(filename, "rb") as file:
-    found = False
-    data = b''
-    byte_count = 0
-
-    # TODO change while True to something with serial
-    while True:
-        byte_of_data = file.read(1)
-        data += byte_of_data
-        byte_count += 1
-        
-        if not found and  MAGIC_WORD in data:
-            magic_word_idx = data.index(MAGIC_WORD)
-            parsed_data = data[magic_word_idx:] 
-            found = True    
-
-            # parse header
-            header = MAGIC_WORD + file.read(HEADER_SIZE - len(MAGIC_WORD))
-            
-            magic_word = header[0:8]
-            version = struct.unpack('<I', header[8:12])[0]
-            total_packet_lenght = struct.unpack('<I', header[12:16])[0]
-            platform = struct.unpack('<I', header[16:20])[0]
-            frame_number = struct.unpack('<I', header[20:24])[0]
-            time_cpu_cycles = struct.unpack('<I', header[24:28])[0]
-            num_of_detected_objects = struct.unpack('<I', header[28:32])[0]            
-            num_of_TLV = struct.unpack('<I', header[32:36])[0]
-            sub_frame_number = struct.unpack('<I', header[36:40])[0]
-
-            # TODO more checks
-            if total_packet_lenght%2 != 0:
-                raise Exception("packet size not devida")
-
-            # get TVL
-            # for i in range(num_of_TLV):
-                # read TVL part
+class Header:
+    def __init__(self, magic_word, version, total_packet_length, platform, frame_number,
+                 time_cpu_cycles, num_of_detected_objects, num_of_TLV, sub_frame_number) -> None:
+        self.magic_word = magic_word
+        self.version = version
+        self.total_packet_length = total_packet_length
+        self.platform = platform
+        self.frame_number = frame_number
+        self.time_cpu_cycles = time_cpu_cycles
+        self.num_of_detected_objects = num_of_detected_objects
+        self.num_of_TLV = num_of_TLV
+        self.sub_frame_number = sub_frame_number
 
 
-
-            pass
-
- 
-        
-
-
-
-    
+class TLV:
+    def __init__(self, type, length, data) -> None:
+        self.type = type
+        self.length = length
+        self.data = data
 
 
+class Frame:
+    def __init__(self, magic_word, version, total_packet_length, platform, frame_number,
+                 time_cpu_cycles, num_of_detected_objects, num_of_TLV, sub_frame_number) -> None:
+        self.header = Header(magic_word, version, total_packet_length, platform, frame_number,
+                             time_cpu_cycles, num_of_detected_objects, num_of_TLV, sub_frame_number)
+        self.tlvs = []
+
+    def append_tvls(self, type, length, data):
+        self.tlvs.append(TLV(type, length, data))
 
 
+class SimualtedSerialPort:
+    FILENAME = "/home/bartek/AGV/AWR1642BOOST_mmWaveRadar/test_data/xwr16xx_processed_stream_2023_10_30T09_00_44_253.dat"
+    MAGIC_WORD = b'\x02\x01\x04\x03\x06\x05\x08\x07'
+    HEADER_SIZE = 40
+    TLV_TYPE_SIZE = 4
+    TLV_LENGTH_SIZE = 4
 
-"""
-import struct
+    def __init__(self):
+        # create FIFO queue for data stream, it will store parsed data Frames
+        self.rx_data_queue = Queue()
 
-# Define the expected header structure
-header_format = '<4H I 3I'
-header_size = struct.calcsize(header_format)
-expected_magic_word = (0x0102, 0x0304, 0x0506, 0x0708)
+    def serial_port_routine(self):
+        with open(self.FILENAME, "rb") as file:
+            found = False
+            data = b''
+            byte_count = 0
 
-# Specify the binary file path
-file_path = 'your_binary_file.bin'
+            # TODO change while True to something with serial
+            while True:
+                byte_of_data = file.read(1)
+                if byte_of_data == '':
+                    print("Reached EOF")
+                    break
 
-# Search for the header in the binary file
-with open(file_path, 'rb') as file:
-    while True:
-        header_data = file.read(header_size)
-        if not header_data:
-            break  # Reached the end of the file
+                data += byte_of_data
+                byte_count += 1
 
-        header = struct.unpack(header_format, header_data)
-        magic_word = header[:4]
-        
-        if magic_word == expected_magic_word:
-            print("Header found at offset:", file.tell() - header_size)
-            print("Magic Word:", magic_word)
-            print("Version:", header[4])
-            print("Total Packet Length:", header[5])
-            print("Platform:", header[6])
-            print("Frame Number:", header[7])
-            print("Time CPU Cycles:", header[8])
-            print("Number of Detected Objects:", header[9])
-            print("Number of TLVs:", header[10])
-            print("Sub-Frame Number:", header[11])
-            break  # You can stop searching after the first header is found
+                if not found and self.MAGIC_WORD in data:
+                    magic_word_idx = data.index(self.MAGIC_WORD)
+                    parsed_data = data[magic_word_idx:]
+                    found = True
 
-"""
+                    # parse header
+                    header = self.MAGIC_WORD + file.read(self.HEADER_SIZE - len(self.MAGIC_WORD))
+                    byte_count += self.HEADER_SIZE - len(self.MAGIC_WORD)
+
+                    magic_word = header[0:8]
+                    version = struct.unpack('<I', header[8:12])[0]
+                    total_packet_length = struct.unpack('<I', header[12:16])[0]
+                    platform = struct.unpack('<I', header[16:20])[0]
+                    frame_number = struct.unpack('<I', header[20:24])[0]
+                    time_cpu_cycles = struct.unpack('<I', header[24:28])[0]
+                    num_of_detected_objects = struct.unpack('<I', header[28:32])[0]
+                    num_of_TLV = struct.unpack('<I', header[32:36])[0]
+                    sub_frame_number = struct.unpack('<I', header[36:40])[0]
+
+                    frame = Frame(magic_word, version, total_packet_length, platform, frame_number,
+                                time_cpu_cycles, num_of_detected_objects, num_of_TLV, sub_frame_number)
+
+                    # TODO more checks
+                    if total_packet_length > 10000:
+                        raise Exception("Exceeded packet length")
+
+                    if total_packet_length % 32 != 0:
+                        raise Exception("Packet size is not a multiple of 32")
+                    found = False
+
+                    # read rest of the frame
+                    frame_tail = frame.read(total_packet_length - byte_count)
+                    byte_count += total_packet_length - byte_count
+
+                    # get individual TVL's
+                    for i in range(num_of_TLV):
+                        # TODO change for frame_tail
+                        tlv_type_packed = frame_tail[0:4]
+                        tlv_type = struct.unpack('<I', tlv_type_packed)[0]
+
+                        # TODO decipher type
+                        tlv_length_packed = file.read(self.TLV_LENGTH_SIZE)
+                        tlv_length = tlv_type = struct.unpack('<I', tlv_length_packed)[0]
+
+                        tlv_data = file.read(tlv_length)
+
+                        frame.append_tvls(tlv_type, tlv_length, tlv_data)
+
+                    pass
+
+
+if __name__ == "__main__":
+    s = SimualtedSerialPort()
+    s.serial_port_routine()
